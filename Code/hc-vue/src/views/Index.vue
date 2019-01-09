179c975b4fb045992f23d7ebe87d147e62fd2c9d
scarecrow <279516548@qq.com>
2019-01-10 00:04:35 +0800

    123

--- Code/hc-vue/src/views/Index.vue
+++ Code/hc-vue/src/views/Index.vue
@@ -1,37 +1,71 @@
 <template>
   <v-app>
     <v-toolbar color="teal darken-1" dark>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-menu :nudge-width="100">
-          <v-toolbar-title slot="activator">
-            <span>All</span>
-            <v-icon dark>arrow_drop_down</v-icon>
-          </v-toolbar-title>
-          <v-list>
-            <v-list-tile
-              v-for="item in items"
-              :key="item"
-            >
-              <v-list-tile-title v-text="item"></v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-menu>
-        <v-spacer>abc</v-spacer>
-        <v-btn icon>
-          <v-icon>search</v-icon>
+      <v-img :src="require('@/assets/imgs/logo1.png')"></v-img>
+      <img src="@/assets/imgs/logo1.png" alt="">
+      <v-menu :nudge-width="100">
+        <v-toolbar-title slot="activator">
+          <span>All</span>
+          <v-icon dark>arrow_drop_down</v-icon>
+        </v-toolbar-title>
+        <v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item"
+          >
+            <v-list-tile-title v-text="item"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-spacer>abc</v-spacer>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn><v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-layout align-center justify-end fill-height>
+        <v-avatar :size="55">
+          <img src="@/assets/imgs/head.png" alt="avatar">
+        </v-avatar>
+        <div class="ml-2">
+          <div><strong>张三李四王五</strong></div>
+          <div><strong>admin</strong></div>
+        </div>
+        <v-btn color="error" flat fab>
+          <v-icon size="30">exit_to_app</v-icon>
         </v-btn>
-        <v-btn icon>
-          <v-icon>favorite</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>more_vert</v-icon>
-        </v-btn>
-        <img class="head-img" src="@/assets/imgs/head.png">
-        <v-btn color="error" small round>
-          admin
-          <v-icon right>arrow_back</v-icon>
-        </v-btn>
-      </v-toolbar>
+      </v-layout>
+    </v-toolbar>
   </v-app>
 </template>
 
